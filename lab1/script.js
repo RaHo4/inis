9f1a719345bcfadcec17bbc530ad797cf1671387
RaHo4 <131150016+RaHo4@users.noreply.github.com>
2024-11-29 13:50:10 +0300

    Small fixes

--- lab1/script.js
+++ lab1/script.js
@@ -1,89 +1,65 @@
-//let shirtsArr = localStorage.getItem("shirts");
-//console.log(typeof shirtsArr)
-shirts.forEach((shirt, index) => {
-    let shirtName = document.createElement("span");
-    shirtName.classList.add("name");
-    shirtName.innerHTML = shirt.name ? shirt.name : "Some name";
-    let shirtDescription = document.createElement("span");
-    shirtDescription.classList.add("info-text");
-    let colorsSize = Object.keys(shirt.colors).length;
-    shirtDescription.innerHTML = shirt.colors ? `Available in ${colorsSize} ${colorsSize == 1 ? "color" : "colors"}` : "Some description";
-    let shirtPhotoe = document.createElement("img");
-    shirtPhotoe.classList.add("photoe");
-    shirtPhotoe.setAttribute('src', shirt.default.front ? shirt.colors.white.front : shirt.default.front);
+const root = document.getElementById("root");
 
-    let container = document.querySelector(".container");
-    
-    let shirtContainer = document.createElement("div");
-    shirtContainer.classList.add("shirt-cont");
-    container.appendChild(shirtContainer);
+const html = document.createElement("div");
+root.classList.add("mega-container");
+html.classList.add("container");
 
-    let photoe = document.createElement("div");
-    photoe.classList.add("photoe");
-    shirtContainer.appendChild(photoe);
-    photoe.appendChild(shirtPhotoe);
-
-    let info = document.createElement("div");
-    info.classList.add("info");
-    shirtContainer.appendChild(info);
-    info.appendChild(shirtName);
-    info.appendChild(shirtDescription);
-
-    let buttons = document.createElement("div");
-    buttons.classList.add("buttons");
-    info.appendChild(buttons);
-
-    let btnView = document.createElement("button");
-    btnView.classList.add("view");
-    btnView.setAttribute("id", index);
-    btnView.innerHTML = "Quick view"
-    buttons.appendChild(btnView);
-
-    let btnSee = document.createElement("button");
-    btnSee.classList.add("see-page");
-    btnSee.setAttribute("id", index);
-    btnSee.innerHTML = "See page"
-    buttons.appendChild(btnSee);
+shirts.forEach((shirt, i) => {
+  const div = document.createElement("div");
+  div.innerHTML = `<figure>
+    <img src=${shirt?.colors?.white?.front || shirt.default.front}>
+    <figcaption>
+      <h2 class="name">${shirt.name}</h2>
+      <div class="info-text">Available in ${
+        Object.keys(shirt.colors).length
+      } colors</div>
+      </figcaption>
+      <div class="buttons">
+      <button class="view" id="${i}">Quick view</button>
+      <button class="see-page" id="${i}">See page</button>
+      </div>
+    </figure>`;
+  html.appendChild(div);
 });
+
+root.appendChild(html);
+
 let megaContainer = document.querySelector(".mega-container");
 let quickViews = document.querySelectorAll(".view");
 let pages = document.querySelectorAll(".see-page");
+
 megaContainer.addEventListener("click", (event) => {
-    for(let view of quickViews){
-        if(event.target == view){
-            openModal(view.getAttribute("id"));
-        }
+  for (let view of quickViews) {
+    if (event.target == view) {
+      openModal(view.getAttribute("id"));
     }
-    for(let page of pages){
-        if(event.target == page ){
-            seePage(page.getAttribute("id"));
-        }
+  }
+  for (let page of pages) {
+    if (event.target == page) {
+      seePage(page.getAttribute("id"));
     }
-})
-function openModal(id){
-    let shirtName = shirts[id].name;
-    let shirtPrice = shirts[id].price;
-    let shirtFront = shirts[id].colors.white.front;
-    let shirtBack = shirts[id].colors.white.back;
-    document.querySelector(".shirt-name").innerHTML = shirtName;
-    document.querySelector(".shirt-price").innerHTML = shirtPrice;
-    document.querySelector("#front").setAttribute("src", shirtFront);
-    document.querySelector("#back").setAttribute("src", shirtBack);
-    let modal = document.querySelector(".modal");
-    modal.showModal();
-    modal.addEventListener("click", () => {
-        if(event.target == modal){
-            modal.close();
-        }
-    })
+  }
+});
+
+function openModal(id) {
+  document.querySelector(".shirt-name").innerHTML = shirts[id].name;
+  document.querySelector(".shirt-price").innerHTML = shirts[id].price;
+
+  document
+    .querySelector("#front")
+    .setAttribute("src", shirts[id].colors.white.front);
+  document
+    .querySelector("#back")
+    .setAttribute("src", shirts[id].colors.white.back);
+
+  let modal = document.querySelector(".modal");
+  modal.showModal();
+  modal.addEventListener("click", () => {
+    modal.close();
+  });
 }
-function seePage(id){
-    // localStorage.setItem("shirtName",shirts[id].name);
-    // localStorage.setItem("shirtDescription",shirts[id].description);
-    // localStorage.setItem("shirtPrice",shirts[id].price);
-    // let colors = shirts[id].colors;
-    // console.log(colors);
-    // localStorage.setItem("shirtColor",colors);
-    localStorage.setItem("shirtInfo", JSON.stringify(shirts[id]));
-    window.location.href = "../lab2/index.html";
+
+function seePage(id) {
+  localStorage.setItem("shirtInfo", JSON.stringify(shirts[id]));
+  window.location.href = "../lab2/index.html";
 }
--- lab2/script.js
+++ lab2/script.js
@@ -1,42 +1,92 @@
 let shirtInfo = JSON.parse(localStorage.getItem("shirtInfo"));
+
+const root = document.getElementById("root");
+
+const html = document.createElement("div");
+html.classList.add("container");
+
+html.innerHTML = `<div class="container">
+      <div class="header">
+        <h1>${shirtInfo.name}</h1>
+      </div>
+      <div class="info-block">
+        <div class="info-photo">
+          <img src="../lab1/${shirtInfo.colors.white.front}" class="photo">
+        </div>
+        <div class="info-text">
+          <span class="price">$${shirtInfo.price}</span>
+          <p class="description">${shirtInfo.description}</p>
+          <div class="mega-buttons-cont">
+            <div class="buttons-cont">
+              <span class="mini-info">Side:</span>
+              <button class="btn" id="front">Front</button>
+              <button class="btn" id="back">Back</button>
+            </div>
+            <div class="buttons-cont" id="color-cont">
+              <span class="mini-info">Color:</span>
+              
+          </div>
+        </div>
+      </div>
+    </div>`;
+root.appendChild(html);
+
 document.querySelector("h1").innerHTML = shirtInfo.name;
-document.querySelector(".photoe").setAttribute("src", `../lab1/${shirtInfo.colors.white.front}`);
+document
+  .querySelector(".photo")
+  .setAttribute("src", `../lab1/${shirtInfo.colors.white.front}`);
 document.querySelector(".price").innerHTML = shirtInfo.price;
 document.querySelector(".description").innerHTML = shirtInfo.description;
 
 let pickedColor = "white";
 let pickedSide = "front";
 
-let buttonsCont = document.querySelector("#color-cont");
+let buttonsContainer = document.querySelector("#color-cont");
 let allButtons = [];
-for(let color in shirtInfo.colors){
-    let colorButton = document.createElement("button");
-    colorButton.classList.add("btn");
-    colorButton.setAttribute("id", `${color}`);
-    colorButton.style.backgroundColor = `${color}`;
-    colorButton.innerHTML = `${color}`;
-    allButtons.push(colorButton);
-    buttonsCont.appendChild(colorButton);
+
+for (let color in shirtInfo.colors) {
+  let colorButton = document.createElement("button");
+
+  colorButton.classList.add("btn");
+  colorButton.setAttribute("id", `${color}`);
+  colorButton.style.backgroundColor = `${color}`;
+  colorButton.innerHTML = `${color}`;
+
+  allButtons.push(colorButton);
+  buttonsContainer.appendChild(colorButton);
 }
-buttonsCont.addEventListener("click", () => {
-    for(let btn of allButtons){
-        if(event.target == btn){
-            let color = btn.getAttribute('id');
-            pickedColor = color;
-            document.querySelector(".photoe").setAttribute("src", `../lab1/${shirtInfo.colors[color][pickedSide]}`)
-        }
+
+buttonsContainer.addEventListener("click", () => {
+  for (let btn of allButtons) {
+    if (event.target == btn) {
+      let color = btn.getAttribute("id");
+      pickedColor = color;
+      document
+        .querySelector(".photo")
+        .setAttribute("src", `../lab1/${shirtInfo.colors[color][pickedSide]}`);
     }
-})  
+  }
+});
 
 let changeSideToFrontBtn = document.querySelector("#front");
 let changeSideToBackBtn = document.querySelector("#back");
 
 changeSideToBackBtn.addEventListener("click", () => {
-    pickedSide = "back";
-    document.querySelector(".photoe").setAttribute("src", `../lab1/${shirtInfo.colors[pickedColor][pickedSide]}`)
-})
+  pickedSide = "back";
+  document
+    .querySelector(".photo")
+    .setAttribute(
+      "src",
+      `../lab1/${shirtInfo.colors[pickedColor][pickedSide]}`
+    );
+});
 
 changeSideToFrontBtn.addEventListener("click", () => {
-    pickedSide = "front";
-    document.querySelector(".photoe").setAttribute("src", `../lab1/${shirtInfo.colors[pickedColor][pickedSide]}`)
-})
+  pickedSide = "front";
+  document
+    .querySelector(".photo")
+    .setAttribute(
+      "src",
+      `../lab1/${shirtInfo.colors[pickedColor][pickedSide]}`
+    );
+});
